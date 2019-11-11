331efe929d5dc95f5392cf9b0851fb4cd1436b5b
aisse-258 <y-kate@ukr.net>
2019-11-11 18:36:42 +0300

    Берет файл и словари, говорит, чего в тексте лишнего

---
+++ pdfreader_compare.js
@@ -0,0 +1,31 @@
+var pdfUtil = require('pdf-to-text');
+var fs = require('fs');
+var word_ext = require('./word_ext.js');
+var clean_text = require('./clean_text.js');
+var dictionary = {};
+var dictionary_main = [];
+process.argv.slice(3).forEach(function (val, index, array){
+    dictionary_main.push(JSON.parse(fs.readFileSync(val, "utf-8")));
+});
+pdfUtil.pdfToText(process.argv[2], function(err, data) {
+    if (err) throw(err);
+    CleanText = clean_text(data);
+    fs.writeFileSync(process.argv[2] + "_text.txt", CleanText, function(err){
+        if(err)
+            return console.log(err);
+    });
+    word_ext(CleanText.toLowerCase(), dictionary);
+    for (let i in dictionary){
+        let exist = false;
+        for (let j in dictionary_main){
+            for (let k in dictionary_main[j]){
+                if (i == k){
+                    exist = true;
+                }
+            }
+        }
+        if (!exist){
+            console.log(i);
+        }
+    }
+});
