bafcf65c8c957e2a6c433af155efb5631c258664
aisse-258 <y-kate@ukr.net>
2019-11-12 15:46:41 +0300

    Сообщать об ошибках нужно до обработки файлов

--- pdfreader_write.js
+++ pdfreader_write.js
@@ -4,9 +4,18 @@ var word_ext = require('./word_ext.js');
 var clean_text = require('./clean_text.js');
 var dictionary = {};
 var counter = 0;
+if(process.argv[2].slice(-5) != '.json'){
+	console.log('WARNING: Dictionary should be a .json file. '+process.argv[2]+' is not .json.');
+}
+if (fs.existsSync(process.argv[2])){
+	console.log('ERROR: File '+process.argv[2]+' is already exists.');
+	process.exit(1);
+}
 process.argv.slice(3).forEach(function(val, index, array) {
 	pdfUtil.pdfToText(val, function(err, data) {
-		if (err) throw(err);
+		if (err){
+			throw(err);
+		}
 		CleanText = clean_text(data);
 		fs.writeFileSync(val + "_text.txt", CleanText, function(err){
 			if(err)
@@ -15,18 +24,11 @@ process.argv.slice(3).forEach(function(val, index, array) {
 		word_ext(CleanText.toLowerCase(), dictionary);
 		counter++;
 		if (counter == process.argv.slice(3).length){
-			if(process.argv[2].slice(-5) != '.json'){
-				console.log('WARNING: Dictionary should be a .json file. '+process.argv[2]+' is not .json.');
-			}
-			if (fs.existsSync(process.argv[2])){
-				console.log('ERROR: File '+process.argv[2]+' is already exists.');
-			}
-			else{
-				fs.writeFile(process.argv[2], JSON.stringify(dictionary), function(err){
-					if(err)
-						return console.log(err);
-				});
-			}
+			fs.writeFile(process.argv[2], JSON.stringify(dictionary), function(err){
+				if(err){
+					return console.log(err);
+				}
+			});
 		}
 	});
 });
