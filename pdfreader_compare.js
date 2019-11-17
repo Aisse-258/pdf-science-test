80dc18d3a212a30394ae70f547af9e7d09928139
aisse-258 <y-kate@ukr.net>
2019-11-17 18:56:14 +0300

    С новым WordExt()

--- pdfreader_compare.js
+++ pdfreader_compare.js
@@ -1,6 +1,7 @@
 var pdfUtil = require('pdf-to-text');
 var fs = require('fs');
-var word_ext = require('./word_ext.js');
+//var word_ext = require('./word_ext.js');
+var WordExt = require('./word_ext_match.js');
 var clean_text = require('./clean_text.js');
 var dictionary = {};
 var dictionary_main = [];
@@ -10,18 +11,17 @@ process.argv.slice(3).forEach(function (val, index, array){
 pdfUtil.pdfToText(process.argv[2], function(err, data) {
     if (err) throw(err);
     CleanText = clean_text(data);
-    fs.writeFileSync(process.argv[2] + "_text.txt", CleanText, function(err){
+    fs.writeFileSync(process.argv[2].slice(0,-4) + ".txt", CleanText, function(err){
         if(err)
             return console.log(err);
     });
-    word_ext(CleanText.toLowerCase(), dictionary);
+    //word_ext(CleanText.toLowerCase(), dictionary);
+    WordExt(data.toLowerCase(), dictionary);
     for (let i in dictionary){
         let exist = false;
         for (let j in dictionary_main){
-            for (let k in dictionary_main[j]){
-                if (i == k){
-                    exist = true;
-                }
+            if (i in dictionary_main[j]) {
+                exist = true;
             }
         }
         if (!exist){
