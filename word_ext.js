51bb2b737d42891e02e49a74031e6c9b3e12e056
aisse-258 <y-kate@ukr.net>
2019-11-17 18:52:26 +0300

    Замена is_repeat встроенными методами

--- word_ext.js
+++ word_ext.js
@@ -1,16 +1,10 @@
-function is_repeat (dictionary, word){
-	for (let i in dictionary)
-		if (i == word)
-			return true;
-	return false;
-}
 var reg_letter = /[^\s“”—–′‘’`\u0001-\u002c \u005b-\u0060 \u002e-\u0040 \u00a1-\u00bf \u007b-\u007e \u20a0-\u20b0 \u2020-\u2027 \u2100-\u214f \u2190-\u2426 \u2900-\u2aff]/;
 var first_word_pos = 0;
 var mem;
 var word_ext = function (data, dictionary) {
 	for (let i = 0; i < data.length; i++){
 		let word = data.slice(first_word_pos, i);
-		if (/^-+$/.test(word)) {
+		if (/^-+$/.test(word) || word == "") {
 			continue;
 		}
 		else if (word[0] == '-'){
@@ -20,7 +14,7 @@ var word_ext = function (data, dictionary) {
 			mem = word.slice(0,-1);
 			continue;
 		}
-		let isRepeat = is_repeat(dictionary, word);
+		let isRepeat = word in dictionary;
 		if (!reg_letter.test(data[i]) && reg_letter.test(data[i-1]) && isRepeat) {
 			dictionary[word]++;
 		}
