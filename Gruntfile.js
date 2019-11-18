377c6b401cd427ec5f604def375a6a8fc4af8ea1
aisse-258 <y-kate@ukr.net>
2019-11-18 19:51:17 +0300

    Конфиги Grunt'а

---
+++ Gruntfile.js
@@ -0,0 +1,258 @@
+'use strict';
+
+var fs = require('fs');
+var ls = require('ls');
+//var regexp = require('node-regexp');
+var mkdirp = require('mkdirp');
+
+var exec = require('child_process').exec;
+
+
+module.exports = function(grunt) {
+
+	require('time-grunt')(grunt);
+	require('load-grunt-tasks')(grunt);
+
+	var cwd = process.cwd();
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		copy: {
+			wrappers: {
+				files: [
+					{
+						expand: true,
+						cwd: 'webui/wrappers',
+						src: ['*.html'],
+						dest: 'dist/'
+					},
+				]
+			},
+			license: {
+				files: [
+					{
+						expand: true,
+						src: ['LICENSE'],
+						dest: 'dist/',
+						rename: function() { return 'dist/LICENSE.TXT';},
+
+					},
+				]
+			},
+			codemirror: {
+				files: [
+					{ expand: true, src: ['node_modules/codemirror/**'], dest: 'dist/webui/' },
+				]
+			},
+			bootstrap: {
+				files: [
+					{ expand: true, src: ['node_modules/bootstrap/**'], dest: 'dist/webui/' },
+				]
+			},
+		},
+		uglify: {
+			options: {
+				screwIE8: true,
+			},
+			head: {
+				files: {
+					'build/lib/head.min.js': ['lib/head.js'],
+				}
+			},
+		},
+		browserify: {
+			options: {
+				browserifyOptions: {
+					debug: true
+				},
+			},
+			main: {
+				src: 'webui/playground.js',
+				dest: 'dist/webui/playground.js'
+			},
+		},
+/*
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1,
+			},
+			target: {
+				files: [
+					{
+						'dist/css/chas-ui.min.css': [
+							 'css/browser.css',
+							 'css/main.css',
+							 'css/menu.css',
+							 'ext/anyslider/css/anythingslider.css',
+							 'ext/anyslider/css/theme-minimalist-square.css',
+							 'ext/fonts/stylesheet.css',
+							 'ext/keyboard/keyboard.css',
+							 'ext/jqplot/jquery.jqplot.css',
+						],
+						'dist/css/chas-ui-bs.min.css': [
+							 'css/browser.css',
+							 'css/menu.css',
+							 'ext/anyslider/css/anythingslider.css',
+							 'ext/anyslider/css/theme-minimalist-square.css',
+							 'ext/fonts/stylesheet.css',
+							 'ext/keyboard/keyboard.css',
+							 'ext/jqplot/jquery.jqplot.css',
+							 'ext/bootstrap/css/bootstrap.min.css',
+						],
+					},
+					{
+						expand: true,
+						cwd: 'css',
+						src: ['*.css'],
+						dest: 'dist/css',
+						ext: '.min.css',
+					}
+				]
+			},
+		},
+*/
+		htmlmin: {
+			options: {
+				removeComments: true,
+				collapseWhitespace: true,
+				conservativeCollapse: true,
+				minifyJS: true,
+				minifyCSS: true,
+			},
+			html: {
+				expand: true,
+				cwd: './',
+				src: ['webui/*.html'],
+				dest: 'dist/',
+				ext: '.html'
+			}
+		},
+		watch: {
+			options: {
+				reload: true
+			},
+			html: {
+				files: [
+					'webui/*.html',
+				],
+				tasks: ['process-html']
+			},
+			webuiJs: {
+				files: [
+					'webui/*.js',
+					'common/*.js',
+				],
+				tasks: ['process-webui-js'],
+			},
+		},
+
+		eslint: {
+			strictLinting: {
+				options: {
+					configFile: 'eslint.json'
+				},
+				src: [
+					'webui/*.js',
+					'common/*.js',
+					'!common/stex-custom.codemirror-mode.js',
+					'cli/*.js',
+					'Gruntfile.js',
+				],
+			},
+		},
+
+		clean: {
+			build: ['build/'],
+			dist: ['dist/']
+		},
+
+		'node-qunit': {
+			Test1: {
+				code: './cli/dictionary_create.js',
+				tests: [
+					'./test/test1.js',
+					'./test/test2.js',
+				],
+				done: function(err, res) {
+					!err && publishResults('node', res, this.async());
+				},
+			},
+			/*Rules: {
+				//deps: 'ls',
+				code: './cli/cli.js',
+				tests: [
+					'./test/Rules.js',
+				],
+				done: function(err, res) {
+					!err && publishResults('node', res, this.async());
+				},
+			},*/
+		},
+		jasmine: {
+            test: {
+                src: [
+					'dist/webui/playground.js',
+				],
+                options: {
+                    vendor: [
+                        'node_modules/jquery/dist/jquery.js',
+                        'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
+						'build/test/webui/makeHTML.js',
+						'dist/webui/node_modules/codemirror/lib/codemirror.js',
+						'dist/webui/node_modules/codemirror/mode/stex/stex.js',
+						'dist/webui/node_modules/codemirror/addon/runmode/runmode.js',
+						'dist/webui/playground.js',
+                    ],
+                    specs: [
+						'test/webui/*.spec.js',
+					],
+					keepRunner: true,
+                }
+            }
+        },
+/*
+		concurrent: {
+			'process-all': [
+				'process-html',
+				'process-webui-js',
+			],
+		},
+*/
+	});
+
+	grunt.registerTask('createHTMLscripts', 'Упаковываем html-код в js-обёртки', function() {
+		require('./test/webui/create-html-making-script.js');
+	});
+
+	grunt.registerTask('testui', [
+		'default',
+		'createHTMLscripts',
+		'jasmine',
+	]);
+
+	grunt.registerTask('process-html', [
+		'newer:htmlmin',
+	]);
+
+	grunt.registerTask('process-webui-js', [
+		'newer:eslint',
+		'browserify:main',
+	]);
+
+	grunt.registerTask('bundle', [
+		'newer:copy:codemirror',
+		'newer:copy:bootstrap',
+		'newer:copy:license',
+		'default',
+		'newer:copy:wrappers',
+		'compress',
+	]);
+
+	grunt.registerTask('default', [
+		//'concurrent:process-all', // На деле - медленнее
+		'node-qunit',
+		'process-html',
+		'process-webui-js',
+	]);
+};
