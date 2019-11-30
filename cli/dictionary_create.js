7b64d1ed9c4d3c42394485ab8b41c86dedf74357
aisse-258 <y-kate@ukr.net>
2019-11-30 15:46:47 +0300

    Работа браузерной библиотеки в консоли

--- cli/dictionary_create.js
+++ cli/dictionary_create.js
@@ -1,33 +1,37 @@
+require('pdfjs-dist/build/pdf.js');
+pdfjsLib = PDFJS;
+pdfjsLib.workerSrc = 'node_modules/pdfjs-dist/build/pdf.worker.js';
+var pdf = require('../common/read_pdf.js');
 var fs = require('fs');
-var pdf = require('../common/pdf_data.js');
-var clean_text = require('../common/clean_text.js').clean_with_replace;
 var word_ext = require('../common/word_ext_match.js');
-var pdfs = [];
-var texts = [];
-var counter = 0;
+var clean_text = require('../common/clean_text.js').clean_with_replace;
 var Dictionary = {};
-if(process.argv[2].slice(-5) != '.json'){
-    console.log('WARNING: Dictionary should be a .json file. '+process.argv[2]+' is not .json.');
-}
-if (fs.existsSync(process.argv[2])){
-    console.log('ERROR: File '+process.argv[2]+' already exists.');
-    process.exit(1);
-}
-var files = process.argv.slice(3);
-for (let i = 0; i < files.length; i++){
-    pdfs.push(fs.readFileSync(files[i]));
-}
-for (let i = 0; i < pdfs.length; i++){
-    pdf(pdfs[i], function(pdfData){
-        texts.push(clean_text(pdfData.data.text));
-        word_ext(texts[i], Dictionary);
-        counter++;
-        if (counter == pdfs.length) {
-            fs.writeFile(process.argv[2], JSON.stringify(Dictionary), function(err){
-                if(err){
+var counter = 0;
+var dictionary_create = function (files, dictionary_name) {
+    if(dictionary_name.slice(-5) != '.json'){
+        console.log('WARNING: Dictionary should be a .json file. '+dictionary_name+' is not .json.');
+    }
+    if (fs.existsSync(dictionary_name)){
+        console.log('ERROR: File '+dictionary_name+' is already exists.');
+        process.exit(1);
+    }
+    for (let i = 0; i < files.length; i++){
+        pdf(pdfjsLib, fs.readFileSync(files[i]), function(text){
+            text = text.normalize('NFKC');
+            fs.writeFileSync(files[i].slice(0,-4) + ".txt", clean_text(text), function(err){
+                if(err)
                     return console.log(err);
-                }
             });
-        }
-    });
+            word_ext(text.toLowerCase(), Dictionary);
+            counter++;
+            if (counter == files.length){
+                fs.writeFile(dictionary_name, JSON.stringify(Dictionary), function(err){
+                    if(err){
+                        return console.log(err);
+                    }
+                });
+            }
+        });
+    }
 }
+dictionary_create(process.argv.slice(3), process.argv[2]);
