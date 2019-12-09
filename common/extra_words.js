ea117c7cee883a7c85b7b6bc8d35db2c73594873
aisse-258 <y-kate@ukr.net>
2019-12-09 17:48:18 +0300

    Слова, редкие в корпусе, а не в тексте

--- common/extra_words.js
+++ common/extra_words.js
@@ -1,18 +1,20 @@
+var dictionary_union = require('./dictionary_union.js');
 var extra_words = function (current_dict, example_dicts, repeat_count) {
     let ext_words = {};
     let rare_words = {};
+    let example_dict = {};
+    if (example_dicts.length == 1) {
+        example_dict = example_dicts[0];
+    }
+    else {
+        example_dict = dictionary_union(example_dicts[0], example_dicts.slice(1));
+    }
     for (let i in current_dict) {
-        let exist = false;
-        for (let j in example_dicts){
-            if (i in example_dicts[j]) {
-                exist = true;
-            }
-        }
-        if (!exist){
+        if (!(i in example_dict)){
             ext_words[i] = current_dict[i];
         }
-        else if (current_dict[i] <= repeat_count) {
-            rare_words[i] = current_dict[i];
+        else if (example_dict[i] <= repeat_count) {
+            rare_words[i] = example_dict[i];
         }
     }
     return {
