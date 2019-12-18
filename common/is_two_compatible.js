802a2affdeaa7a5e1e1b0175fafba148fce6eca6
aisse-258 <y-kate@ukr.net>
2019-12-18 12:45:31 +0300

    Сочетаемость слов: начало(2)

--- common/is_two_compatible.js
+++ common/is_two_compatible.js
@@ -1,15 +1,19 @@
-var is_two_compatible = function(dictionary) {
+var word_count = require('../common/word_count.js');
+var is_two_compatible = function(current_dictionary, ex_dict) {
+    ex_dict.total_two_words = word_count(ex_dict.two_words);
     let not_compatible = {};
-    for (let A_B in dictionary.two_words) {
-        let A = A_B.split(' ')[0], B = A_B.split(' ')[1];
-        let freq_AB = dictionary.two_words[A_B]/dictionary.total_two_words*1.,
-            freq_A = dictionary.words[A]/dictionary.total_words*1.,
-            freq_B = dictionary.words[B]/dictionary.total_words*1.;
-        if(freq_AB < freq_A*freq_B) {
-            not_compatible[A_B] = {
-                [A_B]: Number((freq_AB*100).toFixed(2)),
-                [A]: Number((freq_A*100).toFixed(2)),
-                [B]: Number((freq_B*100).toFixed(2))
+    for (let A_B in current_dictionary.two_words) {
+        if(A_B in ex_dict.two_words) {
+            let A = A_B.split(' ')[0], B = A_B.split(' ')[1];
+            let freq_AB = ex_dict.two_words[A_B]/ex_dict.total_two_words*1.,
+                freq_A = ex_dict.words[A]/ex_dict.total_words*1.,
+                freq_B = ex_dict.words[B]/ex_dict.total_words*1.;
+            if(freq_AB < freq_A*freq_B) {
+                not_compatible[A_B] = {
+                    [A_B]: Number((freq_AB*100).toFixed(2)) + '%',
+                    [A]: Number((freq_A*100).toFixed(2)) + '%',
+                    [B]: Number((freq_B*100).toFixed(2)) + '%'
+                }
             }
         }
     }
