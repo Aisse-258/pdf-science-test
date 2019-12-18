e792ef617abecd94cb95b426c32d23f95861beed
aisse-258 <y-kate@ukr.net>
2019-12-18 12:44:56 +0300

    Правильный порядок слов: начало(2)

--- common/is_in_right_order.js
+++ common/is_in_right_order.js
@@ -1,12 +1,12 @@
-var is_in_right_order = function(dictionary) {
+var is_in_right_order = function(current_dictionary, ex_dict) {
     let rare_B_A = {};
-    for (let A_B in dictionary.two_words) {
+    for (let A_B in current_dictionary.two_words) {
         let A = A_B.split(' ')[0], B = A_B.split(' ')[1];
-        if (B+' '+A in dictionary.two_words) {
-            if(dictionary.two_words[A_B] > dictionary.two_words[B+' '+A]){
+        if (B+' '+A in ex_dict.two_words) {
+            if(ex_dict.two_words[A_B] < ex_dict.two_words[B+' '+A] || (!ex_dict.two_words[A_B] && ex_dict.two_words[B+' '+A])){
                 rare_B_A[A_B] = {
-                    [A+' '+B]: dictionary.two_words[A_B],
-                    [B+' '+A]: dictionary.two_words[B+' '+A]
+                    [A+' '+B]: ex_dict.two_words[A_B],
+                    [B+' '+A]: ex_dict.two_words[B+' '+A]
                 }
             }
         }
