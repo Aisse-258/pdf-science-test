e00557b571d845fe798bf854f712e0ef2a9d29bd
aisse-258 <y-kate@ukr.net>
2019-12-18 12:43:17 +0300

    Работа с объединенным словарем вместо их соединения внутри функции

--- common/extra_words.js
+++ common/extra_words.js
@@ -1,14 +1,6 @@
-var dictionary_union = require('./dictionary_union.js');
-var extra_words = function (current_dict, example_dicts, repeat_count) {
+var extra_words = function (current_dict, example_dict, repeat_count) {
     let ext_words = {};
     let rare_words = {};
-    let example_dict = {};
-    if (example_dicts.length == 1) {
-        example_dict = example_dicts[0];
-    }
-    else {
-        example_dict = dictionary_union(example_dicts[0], example_dicts.slice(1));
-    }
     for (let i in current_dict) {
         if (!(i in example_dict)){
             ext_words[i] = current_dict[i];
