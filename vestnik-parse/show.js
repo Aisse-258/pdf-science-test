dc5a2a52467a606287237aa066ab65a558f154e5
aisse-258 <y-kate@ukr.net>
2020-04-14 12:59:50 +0300

    Их заменяет sb-results

--- vestnik-parse/show.js
+++
@@ -1,7 +0,0 @@
-var words = require('./words'+process.argv[2]+'.json');
-let min_count = process.argv[3], max_count = process.argv[4] || Infinity;
-for (let phrase in words) {
-    if (words[phrase] >= min_count && words[phrase] < max_count) {
-        console.log ('['+words[phrase]+']', phrase);
-    }
-}
