27c1ddae23a1da8347184ee695ea9969f7a2dae5
aisse-258 <y-kate@ukr.net>
2020-05-07 00:58:02 +0300

    Возможность не указывать нижний порог вывода

--- sb-results/n-word-filter/show.js
+++ sb-results/n-word-filter/show.js
@@ -1,5 +1,5 @@
 var words = require('./words'+process.argv[2]+'.json');
-let min_count = process.argv[3], max_count = process.argv[4] || Infinity;
+let min_count = process.argv[3] || 1, max_count = process.argv[4] || Infinity;
 for (let phrase in words) {
     if (words[phrase] >= min_count && words[phrase] < max_count) {
         console.log ('['+words[phrase]+']', phrase);
