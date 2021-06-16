97fa46631bec0f8a3fb098526a8f892adca575b5
aisse-258 <y-kate@ukr.net>
2021-06-16 18:31:44 +0300

    [CLI] Возможность проверить статью в формате .tex

--- cli/compare_dictionaries.js
+++ cli/compare_dictionaries.js
@@ -11,6 +11,7 @@ var extra_words = require('../common/extra_words.js');
 var dictionary_union = require('../common/dictionary_union.js');
 var is_in_right_order = require('../common/is_in_right_order.js');
 var is_two_compatible = require('../common/is_two_compatible.js');
+var tex_cleaner = require('../common/tex_cleaner.js');
 var example_dicts = [];
 var current_dict = new Dictionary();
 for (let i = 0; i < process.argv.slice(4).length; i++){
@@ -50,6 +51,34 @@ if (process.argv[2].slice(-4)=='.pdf') {
 				return console.log(err);
 			}
 		});
+	});
+}
+else if (process.argv[2].slice(-4)=='.tex') {
+    current_dict.text = tex_cleaner(fs.readFileSync(process.argv[2], 'utf-8').normalize('NFKC'));
+	fs.writeFileSync(process.argv[2].slice(0,-4) + "_clean.txt", current_dict.text, function(err){
+		if(err)
+			return console.log(err);
+	});
+	word_ext(current_dict.text.toLowerCase(), current_dict.words);
+	current_dict.two_words = two_word_ext(current_dict.text.toLowerCase());
+	current_dict.word_count();
+	current_dict.clean_f();
+	current_dict.clean_greek();
+	current_dict.repair_broken_words();
+	let repeat_count = process.argv[3];
+	let extraWords = extra_words(current_dict.words, example_dicts.words, repeat_count);
+	let not_rigth_order = is_in_right_order(current_dict, example_dicts);
+	let not_compatible = is_two_compatible(current_dict, example_dicts);
+	console.log('Not found:', extraWords.ExtraWords);
+	if(repeat_count > 0) {
+		console.log('Less than '+repeat_count+' repeats:', extraWords.RareWords);
+	}
+	console.log('Can be not in right order:', not_rigth_order);
+	console.log('Can be not compatible:', not_compatible);
+	fs.writeFile(process.argv[2].slice(0,-4) + "_dict.json", JSON.stringify(current_dict), function(err){
+		if(err){
+			return console.log(err);
+		}
 	});
 }
 else {
